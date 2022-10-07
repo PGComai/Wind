20bbb01ae3a8ca153e8a53efeae4e2b9e9a547ea
PGComai <comai.alex@gmail.com>
2022-10-07 09:14:31 -0700

    worked on papers

--- sail.gd
+++ sail.gd
@@ -12,6 +12,7 @@ var y_cycle = 0
 var rng = RandomNumberGenerator.new()
 var cycle = Vector3.ZERO
 var cycle_factor = 0
+var dist = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,15 +25,10 @@ func _process(delta):
 	#storm = get_parent().get_node("KinematicBody").get_node("Attractor").get_global_transform_interpolated().origin
 	storm = get_parent().get_node("KinematicBody").get_global_transform_interpolated().origin
 	#print(storm)
-	
-	spin_attractor(delta)
-#	$SoftBody.set_point_pinned(pin, true, get_path_to(get_parent_spatial()))
-#	unpin(pin)
-#
-#	pin += 1
-#	if pin > 3:
-#		pin = 0
-	global_translation = global_translation.move_toward(storm + cycle, 0.5)
+	dist = distance_to_storm()
+	if dist < 600:
+		spin_attractor(delta)
+		global_translation = global_translation.move_toward(storm + cycle, 0.5)
 	
 func unpin(p):
 	for x in pins:
@@ -48,3 +44,8 @@ func spin_attractor(delta):
 	var spin_sin = sin(spin_cycle)
 	var spin_cos = cos(spin_cycle)
 	cycle = Vector3(spin_cos + cycle_factor, y_mod + cycle_factor, spin_sin + cycle_factor) * (3 + cycle_factor)
+	
+func distance_to_storm():
+	dist = global_translation.distance_squared_to(storm)
+	#print(dist)
+	return dist
